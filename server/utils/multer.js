ab9c73cb5d34ff614e0fe52ae01ee125fcd86c10
Mr.Haroon <haroonashraf.dev@gmail.com>
2026-03-26 15:57:45 +0500

    update multer.js

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,4 +1,13 @@
-import multer from 'multer';
+import multer from "multer";
 
-const upload = multer({dest:"uploads/"});
+// Use memory storage instead of disk
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // optional: 5MB limit
+  },
+});
+
 export default upload;
